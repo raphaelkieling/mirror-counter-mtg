789329250b5e97494d40df037ec22c5520a96a6d
Raphael Kieling <raphaelkieling98@gmail.com>
2026-08-28 10:35:15 -0300

    chore: vibration

--- components/life-counter.tsx
+++ components/life-counter.tsx
@@ -80,8 +80,12 @@ export default function LifeCounter() {
   const activePlayer = useMemo(() => players.find((player) => player.id === settingsId), [players, settingsId])
 
   function changeLife(id: number, delta: number) {
-    if ('vibrate' in navigator) {
-      navigator.vibrate(30)
+    try {
+      if (navigator.vibrate) {
+        navigator.vibrate([20, 10, 20])
+      }
+    } catch (e) {
+      console.log('Vibration not available')
     }
     setPlayers((current) => current.map((player) => player.id === id ? { ...player, life: Math.max(0, player.life + delta) } : player))
     window.clearTimeout(historyTimers.current[id])
